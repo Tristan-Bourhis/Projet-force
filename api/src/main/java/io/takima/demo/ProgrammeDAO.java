560a10ac33f115acdf7227f29d2475fd3c050ccf
Anatole Durré <anatole.durre@hotmail.com>
2022-10-10 11:57:59 +0200

    fusion front back

--- api/src/main/java/io/takima/demo/ProgrammeDAO.java
+++ api/src/main/java/io/takima/demo/ProgrammeDAO.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProgrammeDAO extends CrudRepository<Programme, Long> {
 
-    @Query()
-    Iterable<Seance> findAllSeance();
+
+
 }
