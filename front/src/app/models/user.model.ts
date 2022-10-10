e5b3a448dfbae3ee1da80bacb420033b703c71d2
Tristan Bourhis <tristan.bourhis@epfedu.fr>
2022-10-10 16:48:51 +0200

    modify user.model

--- front/src/app/models/user.model.ts
+++ front/src/app/models/user.model.ts
@@ -4,7 +4,10 @@ export class User {
   id: number;
   firstName: string;
   lastName: string;
+  email: string;
   age: number;
+  taille: number;
+  poids: number;
 
   constructor(user?: Partial<User>) {
     defaultsDeep(this, user);
