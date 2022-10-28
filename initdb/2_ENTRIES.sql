c251839cec5cedf06cf7a15b351d619db7748fe4
Tristan BOURHIS <76966382+Tristan-Bourhis@users.noreply.github.com>
2022-10-29 00:01:25 +0200

    Delete initdb directory

--- initdb/2_ENTRIES.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO defaultdb.users (id, first_name, last_name, email) VALUES (1, 'Paul', 'Harrohide', 'paul@gmail.com');
-INSERT INTO defaultdb.users (id, first_name, last_name, email) VALUES (2, 'Harry', 'Covert', 'HarryCovert@gmail.com');
